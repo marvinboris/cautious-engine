fe0f343a69c7e6eb0618a2ebdda041d7e274b1f3
Brainer21 <jarisl.ultio.21@gmail.com>
2021-09-09 06:11:09 +0100

    Made the enrolment form easier

--- resources/js/src/components/Error/Error.js
+++ resources/js/src/components/Error/Error.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const error = ({ err }) => (err ? <div className="alert alert-danger text-12 text-md-14 text-xxl-16">{err.message ? err.message : err}</div> : null);
+const error = ({ err }) => (err ? <div className="alert alert-danger text-10 text-md-12 text-xxl-14">{err.message ? err.message : err}</div> : null);
 
 export default error;
